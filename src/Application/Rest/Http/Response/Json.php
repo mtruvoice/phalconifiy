c92d6d28f1b7224b6e93f404e1b9ff600e8298a4
P Kalaytan <p@kalaytan.com>
2017-07-12 13:14:59 +0100

    default text added to responses

--- src/Application/Rest/Http/Response/Json.php
+++ src/Application/Rest/Http/Response/Json.php
@@ -11,12 +11,12 @@ class Json extends General implements ResponseInterface
      * Sets an error JSON response.
      *
      * @param array $messages Messages to show to the user
-     * @param mixed $payload  Additional payload to return, such objects, etc
-     * @param int   $code     Return status code
+     * @param mixed $payload Additional payload to return, such objects, etc
+     * @param int $code Return status code
      *
      * @return ResponseInterface
      */
-    public static function error(array $messages, $payload = null, int $code = self::CODE_BAD_REQUEST)
+    public static function error(array $messages = ['error'], $payload = null, int $code = self::CODE_BAD_REQUEST)
     {
         $response = new static();
         $response->setStatusCode($code, self::CODE_BAD_REQUEST);
@@ -32,11 +32,11 @@ class Json extends General implements ResponseInterface
      * Returns  Denied JSON error response. Denied = not enough privileges-permissions
      *                           - server understood the request but denies the access
      * @param array $messages Message to show to user
-     * @param null  $payload  Additional payload to return
-     * @param int   $code     Return status code
+     * @param null $payload Additional payload to return
+     * @param int $code Return status code
      * @return ResponseInterface
      */
-    public static function denied(array $messages, $payload = null, int $code = self::CODE_DENIED)
+    public static function denied(array $messages = ['access denied'], $payload = null, int $code = self::CODE_DENIED)
     {
         $response = new static();
         $response->setStatusCode($code, self::ERROR_PERMISSION_DENIED);
@@ -52,11 +52,11 @@ class Json extends General implements ResponseInterface
      * Returns Unauthorized JSON error response. Unauthorized = not logged in.
      *
      * @param array $messages Message to show to user
-     * @param null  $payload  Additional payload to return
-     * @param int   $code     Return status code
+     * @param null $payload Additional payload to return
+     * @param int $code Return status code
      * @return ResponseInterface
      */
-    public static function unauthorized(array $messages, $payload = null, int $code = self::CODE_UNAUTHORIZED)
+    public static function unauthorized(array $messages = ['access unauthorized'], $payload = null, int $code = self::CODE_UNAUTHORIZED)
     {
         $response = new static();
         $response->setStatusCode($code, self::ERROR_UNAUTHORIZED);
@@ -72,11 +72,11 @@ class Json extends General implements ResponseInterface
      * Sets a success JSON response.
      *
      * @param array $messages Messages to show to the user
-     * @param mixed $payload  Additional payload to return, such objects, etc
-     * @param int   $code     Return status code
+     * @param mixed $payload Additional payload to return, such objects, etc
+     * @param int $code Return status code
      * @return ResponseInterface
      */
-    public static function success(array $messages, $payload = null, int $code = self::CODE_OK)
+    public static function success(array $messages = ['success'], $payload = null, int $code = self::CODE_OK)
     {
         $response = new static();
         $response->setStatusCode($code);
@@ -94,7 +94,7 @@ class Json extends General implements ResponseInterface
      * @param array $messages Messages to show to the user
      * @return ResponseInterface
      */
-    public static function notFound(array $messages)
+    public static function notFound(array $messages = ['not found'])
     {
         $response = new static();
         $response->setStatusCode(self::CODE_NOT_FOUND);
